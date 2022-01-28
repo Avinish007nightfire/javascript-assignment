aa3d440b44953f8d836553a869d53b6bf50be858
Avinish007nightfire <91722454+Avinish007nightfire@users.noreply.github.com>
2022-01-28 13:05:18 +0530

    Delete Area of triangle.js

--- Area of triangle.js
+++
@@ -1,8 +0,0 @@
-var base = parseInt(prompt("Enter the base: "));
-var height = parseInt(prompt("Enter the height: "));
-
-var area = (base * height) / 2;
-
-console.log("Base: " + base);
-console.log("Height: " + height);
-console.log("The area of the triangle is " + area);
