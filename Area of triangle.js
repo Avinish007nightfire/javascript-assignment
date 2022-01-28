c5c337412e0ed8d67c2ce3ff794bb1fd42a8937a
Avinish007nightfire <91722454+Avinish007nightfire@users.noreply.github.com>
2022-01-28 13:05:37 +0530

    Add files via upload

---
+++ Area of triangle.js
@@ -0,0 +1,7 @@
+var base = 4
+var height = 5
+var area = (base * height) / 2;
+
+console.log("Base: " + base);
+console.log("Height: " + height);
+console.log("The area of the triangle is " + area);
